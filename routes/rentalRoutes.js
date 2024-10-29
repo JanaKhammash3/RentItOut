3658bb5c4032113ba61ae5d739c49f251bfccef7
ZainAbuBaker <s12027614@stu.najah.edu>
2024-10-29 14:26:07 +0200

    last

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -9,9 +9,9 @@ console.log(rentalController);
 router.post('/',authMiddleware(),rentalController.startRental);
 router.get('/',  authMiddleware(), rentalController.getRentals); // Auth required
 
-//router.put('/rentals/:rentalId', authMiddleware(['admin']), rentalController.updateRental);
-router.put('/:rentalId',  rentalController.updateRental);
+router.put('/:rentalId', authMiddleware(['admin']), rentalController.updateRental);
 
-//router.delete('/rentals/:rentalId', authMiddleware(['admin']), rentalController.cancelRental);
-router.delete('/:rentalId',  rentalController.cancelRental);
+
+router.delete('/:rentalId', authMiddleware(['admin']), rentalController.cancelRental);
+
 module.exports = router;
